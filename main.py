7349d4543f58cb5ebf1638ca67214021c007441f
Sebastian Waisbrot <seppo0010@gmail.com>
2023-02-26 21:45:34 -0300

    Event description

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import sys
 import requests
 import os
+import re
 import shutil
 import html
 from datetime import timedelta
 import dateutil.parser
 from ics import Calendar, Event
 from bs4 import BeautifulSoup
+from markdownify import markdownify as md
 
 OUT_DIR = os.environ.get('OUT_DIR', './out')
 BASE_URL = os.environ.get('BASE_URL', 'http://www.derecho.uba.ar/')
 IN_URL = os.environ.get('IN_URL', 'http://www.derecho.uba.ar/agenda/')
-debug = os.environ.get('DEBUG', 'false') == 'true'
-if debug:
-    with open('example.html') as fp:
-        html_doc = fp.read()
-else:
-    req = requests.get(IN_URL)
-    html_doc = req.text
+req = requests.get(IN_URL)
+html_doc = req.text
+
+def get_description(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    el = soup.find('div', class_='list-aviso')
+    if el is None:
+        return url
+    return re.sub(r'\n+', '\n', md(str(el)))
 
 calendar = Calendar()
 soup = BeautifulSoup(html_doc, 'html.parser')
@@ -25,10 +30,12 @@ for article in soup.find_all('article', class_='item-agenda'):
     fecha = article.find('time')['datetime']
     link = article.find('a')
     title = link.get_text()
+    url = BASE_URL + link['href']
     calendar.events.add(Event(
         name=title,
         begin=fecha + '-03:00',
-        description=BASE_URL + link['href'],
+        description=get_description(url),
+        url=url,
         duration=timedelta(hours=2),
     ))
 
@@ -50,7 +57,7 @@ with open(f'{OUT_DIR}/index.html', 'w') as f:
         f.write('<tr>\n')
         f.write(f'<td>{ev.name}</td>\n')
         f.write(f'<td>{dateutil.parser.isoparse(str(ev.begin)).strftime("%d/%m/%y %H:%M")}</td>\n')
-        f.write(f'<td><a href="{ev.description}">Más información</a></td>\n')
+        f.write(f'<td><a href="{ev.url}">Más información</a></td>\n')
         f.write('</tr>\n')
     f.write('</table>\n')
     f.write('</body>\n')
